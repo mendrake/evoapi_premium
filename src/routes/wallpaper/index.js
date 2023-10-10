7385bd55183fdf5c051b6de25a9cdc81f332145e
bruno918 <bruno-leal@educar.rs.gov.br>
2023-10-10 17:11:27 -0300

    remove satanic wallpaper

--- src/routes/wallpaper/index.js
+++ src/routes/wallpaper/index.js
@@ -7,12 +7,12 @@ const app = express.Router();
 const cyber = require("./cyberSpace/cyber");
 const game = require("./gamerWallpaper/gamerwallpaper");
 const aes = require("./aesthetic/aes");
-const sat = require("./satanic/sat");
+
 
 app.use("/cyber", cyber);
 app.use("/gamewallpaper", game);
 app.use("/aesthetic", aes);
-app.use("/satanic", sat);
+
 
 app.get("/", async (req, res) => {
   res.status(200).json({ msg: "Rota para as apis de wallpaper" });
